1d3a8989d13ce27e10f5e6bb67708099a29d4a65
aero568 <aero568@protonmail.com>
2024-07-19 08:50:51 +0100

    Feasible region plot

--- plotfun.py
+++ plotfun.py
@@ -1,13 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-def set_plot(f,xlim=[-5,5],ylim=[-4,8]):
+
+def set_plot(f, xlim=[-5, 5], ylim=[-4, 8]):
 
     # design variables
-    x1 = np.arange(-10, 10.1, 0.05)
-    x2 = np.arange(-10, 10.1, 0.05)
+    x1 = np.arange(-10, 10.1, 0.01)
+    x2 = np.arange(-10, 10.1, 0.01)
     X1, X2 = np.meshgrid(x1, x2)
-    
+
     # Defining contour levels
     levels = np.linspace(-40, 40, 40)
 
@@ -18,3 +19,11 @@ def set_plot(f,xlim=[-5,5],ylim=[-4,8]):
     plt.xlim(xlim)
     plt.ylim(ylim)
     plt.contour(X1, X2, f(X1, X2), levels=levels)
+
+    plt.imshow(
+        ((X1**2 + X2**2 <= 1) & ((X1 - 3 * X2 + 0.5) >= 0)).astype(int),
+        extent=(X1.min(), X1.max(), X2.min(), X2.max()),
+        origin="lower",
+        cmap="Greys",
+        alpha=0.3,
+    )
