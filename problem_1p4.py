31bbd437f14d52ad45f3cee5aeae603e10c7ffb4
aero568 <aero568@protonmail.com>
2024-08-04 16:52:29 +0100

    Explicit functions replace by components

--- problem_1p4.py
+++ problem_1p4.py
@@ -1,26 +1,17 @@
 import matplotlib.pyplot as plt
+import numpy as np
 import openmdao.api as om
+from omxdsm import write_xdsm
 
 from functions import f1p4 as f
+from functions import g1, g2
 from plotfun import set_plot
 
 # build the model
 prob = om.Problem()
-prob.model.add_subsystem(
-    "function",
-    om.ExecComp("obj = (1 - x1) ** 2 + (1 - x2) ** 2 + 0.5 * (2 * x2 - x1**2) ** 2"),
-    promotes=["x1", "x2","obj"],
-)
-prob.model.add_subsystem(
-    "constraint1",
-    om.ExecComp("g1 = x1**2+x2**2"),
-    promotes=["x1", "x2", "g1"],
-)
-prob.model.add_subsystem(
-    "constraint2",
-    om.ExecComp("g2 = x1-3*x2+0.5"),
-    promotes=["x1", "x2", "g2"],
-)
+prob.model.add_subsystem("function", f(), promotes=["*"])
+prob.model.add_subsystem("constraint_1", g1(), promotes=["*"])
+prob.model.add_subsystem("constraint_2", g2(), promotes=["*"])
 
 # setup the optimization
 prob.driver = om.ScipyOptimizeDriver()
@@ -29,18 +20,14 @@ prob.model.add_design_var("x1", lower=0)
 prob.model.add_design_var("x2", lower=0)
 prob.model.add_constraint("g1", upper=1.0)
 prob.model.add_constraint("g2", lower=0.0)
-prob.model.add_objective("obj")
+prob.model.add_objective("y")
 prob.setup()
-
-# set initial values
-prob.set_val("x1", 0.0)
-prob.set_val("x2", 0.0)
 
 # run the optimization
 prob.run_driver()
 
 # minimum value
-print(prob.get_val("function.obj"))
+print(prob.get_val("function.y"))
 
 # location of the minimum
 x1_opt = prob.get_val("x1")
@@ -48,9 +35,21 @@ x2_opt = prob.get_val("x2")
 print(x1_opt)
 print(x2_opt)
 
-
 # plot location of the minimum over contour
-set_plot(f)
+set_plot(f,g1,g2)
 plt.plot(x1_opt, x2_opt, "bo")
-# plt.imshow(g1<=1)
-plt.show()
+#plt.show()
+plt.savefig('two-dimensional.pdf')
+
+# xsdm diagram
+write_xdsm(
+    prob,
+    filename="two-dimensional-xdsm",
+    out_format="pdf",
+    show_browser=False,
+    quiet=True,
+    output_side="left",
+    include_indepvarcomps=False,
+    class_names=False,
+    numbered_comps=False,
+)
