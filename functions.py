e5d16cf85e8c60d6acf5f54295989cb1306651d9
aero568 <aero568@protonmail.com>
2024-08-04 16:49:32 +0100

    Functions and constraints as openmdao explicit component

--- functions.py
+++ functions.py
@@ -1,26 +1,64 @@
-import numpy as np
 import openmdao.api as om
-from bokeh.models import inputs
+
+class g1(om.ExplicitComponent):
+
+    def setup(self):
+
+        # global design variables
+        self.add_input("x1", val=0.0)
+        self.add_input("x2", val=0.0)
+
+        # output setup
+        self.add_output("g1", val=0.0)
+
+    def setup_partials(self):
+
+        # finite difference all partials
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs):
+
+        # evaluate example 1.4 function
+        x1 = inputs["x1"]
+        x2 = inputs["x2"]
+
+        outputs["g1"] = x1**2 + x2**2
 
 
-def f1p2(x1, x2):
-    y = x1**3 + 2 * x1 * x2**2 - x2**3 - 20 * x1
-    return y
+class g2(om.ExplicitComponent):
 
+    def setup(self):
 
-def g1(x1, x2):
-    y = x1**2 + x2**2
-    return y
+        # global design variables
+        self.add_input("x1", val=0.0)
+        self.add_input("x2", val=0.0)
+
+        # output setup
+        self.add_output("g2", val=0.0)
+
+    def setup_partials(self):
+
+        # finite difference all partials
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs):
+
+        # evaluate example 1.4 function
+        x1 = inputs["x1"]
+        x2 = inputs["x2"]
+
+        outputs["g2"] = x1 - 3 * x2 + 0.5
 
 
 class f1p4(om.ExplicitComponent):
 
     def setup(self):
 
-        #global design variables
-        self.add_input("x", val=np.ones(2))
+        # global design variables
+        self.add_input("x1", val=0.0)
+        self.add_input("x2", val=0.0)
 
-        #output setup
+        # output setup
         self.add_output("y", val=1.0)
 
     def setup_partials(self):
@@ -31,7 +69,7 @@ class f1p4(om.ExplicitComponent):
     def compute(self, inputs, outputs):
 
         # evaluate example 1.4 function
-        x1 = inputs["x"][0]
-        x2 = inputs["x"][1]
+        x1 = inputs["x1"]
+        x2 = inputs["x2"]
 
         outputs["y"] = (1 - x1) ** 2 + (1 - x2) ** 2 + 0.5 * (2 * x2 - x1**2) ** 2
